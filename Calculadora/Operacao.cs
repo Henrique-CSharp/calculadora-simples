4f0b2f1af2d02b58616bbd51032d403483c931db
Henrique-CSharp <pedrodinizlui@gmail.com>
2025-09-22 15:24:59 -0300

    subindo novas funcionalidades

--- Calculadora/Operacao.cs
+++ Calculadora/Operacao.cs
@@ -11,6 +11,7 @@ namespace Calculadora
     {
         public double A;
         public double B;
+        public int C;
 
         public double Subtracao(double a, double b)
         {
@@ -31,6 +32,34 @@ namespace Calculadora
         {
            return a / b;
         }
+
+        public double Potencia(double a, double b)
+        {
+           return Math.Pow(a, b);
+        }
+
+        public double Raiz(double a)
+        {
+           return Math.Sqrt(a);
+        }
+
+        public int Fatorial(int a)
+        { 
+           int resultado = 1;
+           for (int i = a; i > 1; i--)
+             
+               resultado = resultado * i;
+
+        return resultado;
+
+            }
+            
+
+        }
+
         
-    }
+
+     
+        
+    
 }
--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -25,28 +25,48 @@ namespace Calculadora
                 Console.WriteLine("2. Adição");
                 Console.WriteLine("3. Multiplicação");
                 Console.WriteLine("4. Divisão");
+                Console.WriteLine("5. Potência");
+                Console.WriteLine("6. Raíz Quadrada");
+                Console.WriteLine("7. Fatorial");
+
                 opcao = int.Parse(Console.ReadLine());
-                Console.WriteLine("Entre com os números:");
-                
-                o.A = double.Parse(Console.ReadLine());
-                o.B = double.Parse(Console.ReadLine());
-                
+                       
                 switch (opcao)
                 {
                     case 1:
+                        
+                        Console.Clear();
+                        Console.WriteLine("Entre com os números:");
+                        o.A = double.Parse(Console.ReadLine());
+                        o.B = double.Parse(Console.ReadLine());
                         Console.WriteLine($"{o.A} - {o.B} = {o.Subtracao(o.A, o.B)}");
                         break;
                     
                     case 2:
+                        
+                        Console.Clear();
+                        Console.WriteLine("Entre com os números:");
+                        o.A = double.Parse(Console.ReadLine());
+                        o.B = double.Parse(Console.ReadLine());
                         Console.WriteLine($"{o.A} + {o.B} = {o.Soma(o.A, o.B)}");
                         break;
                     
                     case 3:
+                        
+                        Console.Clear();
+                        Console.WriteLine("Entre com os números:");
+                        o.A = double.Parse(Console.ReadLine());
+                        o.B = double.Parse(Console.ReadLine());
                         Console.WriteLine($"{o.A} * {o.B} = {o.Multiplicacao(o.A, o.B)}");
                         break;
                     
                     case 4:
                         
+                        Console.Clear();
+                        Console.WriteLine("Entre com os números:");
+                        o.A = double.Parse(Console.ReadLine());
+                        o.B = double.Parse(Console.ReadLine());
+
                         if (o.A == 0 || o.B == 0) 
                         {
 
@@ -60,10 +80,47 @@ namespace Calculadora
                              break;
                         }
 
-                    default:
-                            Console.WriteLine("Voltando ao menu principal.");
+                    case 5:
+                        
+                        Console.Clear();
+                        Console.WriteLine("Entre com os números:");
+                        o.A = double.Parse(Console.ReadLine());
+                        o.B = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine($"A {o.A} elevado {o.B} é {o.Potencia(o.A, o.B).ToString("F2", CultureInfo.InvariantCulture)}");
                         break;
+
+                    case 6:
+                        
+                        Console.Clear();
+                        Console.WriteLine("Entre com o número:");
+                        o.A = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine($"A raíz quadrada de {o.A} é {o.Raiz(o.A).ToString("F2", CultureInfo.InvariantCulture)}");
+                        break;
+                    
+                    case 7:
+                        
+                        Console.Clear();
+                        Console.WriteLine("Entre com o número:");
+                        o.C = int.Parse(Console.ReadLine());
+
+                        if (o.C == 0)
+                        {
+                            Console.WriteLine("É impossível calcular o fatorial de 0.");
+                            break;
+                        }
+                        else { 
+                       
+                            Console.WriteLine($"{o.C}! é {o.Fatorial(o.C)}");
+                            break;
+
+                        }
+            
                 }
+                Console.WriteLine("Pressione qualquer tecla.");
+                Console.ReadKey();
+                Console.Clear();
                 Console.WriteLine("Pressione 1 para voltar ao munu de operações ou 0 para sair.");
                 confirmacao = int.Parse(Console.ReadLine());
 
